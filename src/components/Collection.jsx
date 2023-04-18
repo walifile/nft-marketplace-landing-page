151587ccdacb02bdcfb742d0f07a9f2a0c53a1c1
cherrybytedev <89452621+walifile@users.noreply.github.com>
2023-04-19 04:25:58 +0500

    feat : collection-slider

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import { collection } from "../data";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Collection = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  };
   return (
     <>
-      <section
-        className="section top-collection"
-        aria-labelledby="collection-label"
-      >
+      <section className="section top-collection">
         <div className="container">
           <div className="title-wrapper">
-            <h2
-              className="headline-md section-title text-center"
-              id="collection-label"
-            >
+            <h2 className="headline-md section-title text-center">
               Top Collections
             </h2>
 
@@ -23,328 +28,69 @@ const Collection = () => {
             </a>
           </div>
 
-          <div className="slider" data-slider>
-            <ul className="slider-container" data-slider-container>
-              <li className="slider-item">
-                <div className="collection-card card">
-                  <figure
-                    className="card-banner img-holder"
-                    // style="--width: 500; --height: 500;"
-                    // style={{--width: 500}}
-                  >
-                    <img
-                      src="./assets/images/showcase-1.jpg"
-                      width="500"
-                      height="500"
-                      loading="lazy"
-                      alt="Digital Collection"
-                      className="img-cover"
-                    />
-                  </figure>
+          <div className="slider_wrapper">
+            <ul className="slider_container">
+              <Slider {...settings}>
+                {collection.map((card, index) => (
+                  <li className="slider-item">
+                    <div className="collection-card card">
+                      <figure className="card-banner img-holder">
+                        <img
+                          src={card?.featured_image}
+                          width="500"
+                          height="500"
+                          loading="lazy"
+                          alt="Animations Collection"
+                          className="img-cover"
+                        />
+                      </figure>
 
-                  <div className="card-content">
-                    <div className="card-profile">
-                      <img
-                        src="./assets/images/avatar-1.jpg"
-                        width="64"
-                        height="64"
-                        loading="lazy"
-                        alt="CutieGirl profile"
-                      />
+                      <div className="card-content">
+                        <div className="card-profile">
+                          <img
+                            src={card?.avatar}
+                            width="64"
+                            height="64"
+                            loading="lazy"
+                            alt="CrazyAnyone profile"
+                          />
 
-                      <ion-icon
-                        name="checkmark-circle"
-                        aria-hidden="true"
-                      ></ion-icon>
+                          <ion-icon
+                            name="checkmark-circle"
+                            aria-hidden="true"
+                          ></ion-icon>
+                        </div>
+
+                        <h3 className="title-md card-title">
+                          <a href="#" className="link:hover">
+                            {card?.name}
+                          </a>
+                        </h3>
+
+                        <p className="label-md card-author">
+                          by{" "}
+                          <a href="#" className="link">
+                            {card?.creator}
+                          </a>
+                        </p>
+                        <p className="card-text">{card?.items} Items</p>
+                      </div>
                     </div>
-
-                    <h3 className="title-md card-title">
-                      <a href="#" className="link:hover">
-                        Digital Collection
-                      </a>
-                    </h3>
-
-                    <p className="label-md card-author">
-                      by{" "}
-                      <a href="#" className="link">
-                        @CutieGirl
-                      </a>
-                    </p>
-
-                    <p className="card-text">25 Items</p>
-                  </div>
-                </div>
-              </li>
-
-              <li className="slider-item">
-                <div className="collection-card card">
-                  <figure
-                    className="card-banner img-holder"
-                    // style="--width: 500; --height: 500;"
-                  >
-                    <img
-                      src="./assets/images/showcase-2.jpg"
-                      width="500"
-                      height="500"
-                      loading="lazy"
-                      alt="GIF Collection"
-                      className="img-cover"
-                    />
-                  </figure>
-
-                  <div className="card-content">
-                    <div className="card-profile">
-                      <img
-                        src="./assets/images/avatar-2.jpg"
-                        width="64"
-                        height="64"
-                        loading="lazy"
-                        alt="ButterFly profile"
-                      />
-
-                      <ion-icon
-                        name="checkmark-circle"
-                        aria-hidden="true"
-                      ></ion-icon>
-                    </div>
-
-                    <h3 className="title-md card-title">
-                      <a href="#" className="link:hover">
-                        GIF Collection
-                      </a>
-                    </h3>
-
-                    <p className="label-md card-author">
-                      by{" "}
-                      <a href="#" className="link">
-                        @ButterFly
-                      </a>
-                    </p>
-
-                    <p className="card-text">25 Items</p>
-                  </div>
-                </div>
-              </li>
-
-              <li className="slider-item">
-                <div className="collection-card card">
-                  <figure
-                    className="card-banner img-holder"
-                    // style="--width: 500; --height: 500;"
-                  >
-                    <img
-                      src="./assets/images/showcase-3.jpg"
-                      width="500"
-                      height="500"
-                      loading="lazy"
-                      alt="Sports Collection"
-                      className="img-cover"
-                    />
-                  </figure>
-
-                  <div className="card-content">
-                    <div className="card-profile">
-                      <img
-                        src="./assets/images/avatar-3.jpg"
-                        width="64"
-                        height="64"
-                        loading="lazy"
-                        alt="NorseQueen profile"
-                      />
-
-                      <ion-icon
-                        name="checkmark-circle"
-                        aria-hidden="true"
-                      ></ion-icon>
-                    </div>
-
-                    <h3 className="title-md card-title">
-                      <a href="#" className="link:hover">
-                        Sports Collection
-                      </a>
-                    </h3>
-
-                    <p className="label-md card-author">
-                      by{" "}
-                      <a href="#" className="link">
-                        @NorseQueen
-                      </a>
-                    </p>
-
-                    <p className="card-text">25 Items</p>
-                  </div>
-                </div>
-              </li>
-
-              <li className="slider-item">
-                <div className="collection-card card">
-                  <figure
-                    className="card-banner img-holder"
-                    // style="--width: 500; --height: 500;"
-                  >
-                    <img
-                      src="./assets/images/showcase-4.jpg"
-                      width="500"
-                      height="500"
-                      loading="lazy"
-                      alt="Photography Collection"
-                      className="img-cover"
-                    />
-                  </figure>
-
-                  <div className="card-content">
-                    <div className="card-profile">
-                      <img
-                        src="./assets/images/avatar-4.jpg"
-                        width="64"
-                        height="64"
-                        loading="lazy"
-                        alt="BigBull profile"
-                      />
-
-                      <ion-icon
-                        name="checkmark-circle"
-                        aria-hidden="true"
-                      ></ion-icon>
-                    </div>
-
-                    <h3 className="title-md card-title">
-                      <a href="#" className="link:hover">
-                        Photography Collection
-                      </a>
-                    </h3>
-
-                    <p className="label-md card-author">
-                      by{" "}
-                      <a href="#" className="link">
-                        @BigBull
-                      </a>
-                    </p>
-
-                    <p className="card-text">25 Items</p>
-                  </div>
-                </div>
-              </li>
-
-              <li className="slider-item">
-                <div className="collection-card card">
-                  <figure
-                    className="card-banner img-holder"
-                    // style="--width: 500; --height: 500;"
-                  >
-                    <img
-                      src="./assets/images/showcase-5.jpg"
-                      width="500"
-                      height="500"
-                      loading="lazy"
-                      alt="Illustrations Collection"
-                      className="img-cover"
-                    />
-                  </figure>
-
-                  <div className="card-content">
-                    <div className="card-profile">
-                      <img
-                        src="./assets/images/avatar-5.jpg"
-                        width="64"
-                        height="64"
-                        loading="lazy"
-                        alt="Angel profile"
-                      />
-
-                      <ion-icon
-                        name="checkmark-circle"
-                        aria-hidden="true"
-                      ></ion-icon>
-                    </div>
-
-                    <h3 className="title-md card-title">
-                      <a href="#" className="link:hover">
-                        Illustrations Collection
-                      </a>
-                    </h3>
-
-                    <p className="label-md card-author">
-                      by{" "}
-                      <a href="#" className="link">
-                        @Angel
-                      </a>
-                    </p>
-
-                    <p className="card-text">25 Items</p>
-                  </div>
-                </div>
-              </li>
-
-              <li className="slider-item">
-                <div className="collection-card card">
-                  <figure
-                    className="card-banner img-holder"
-                    // style="--width: 500; --height: 500;"
-                  >
-                    <img
-                      src="./assets/images/showcase-6.gif"
-                      width="500"
-                      height="500"
-                      loading="lazy"
-                      alt="Animations Collection"
-                      className="img-cover"
-                    />
-                  </figure>
-
-                  <div className="card-content">
-                    <div className="card-profile">
-                      <img
-                        src="./assets/images/avatar-6.gif"
-                        width="64"
-                        height="64"
-                        loading="lazy"
-                        alt="CrazyAnyone profile"
-                      />
-
-                      <ion-icon
-                        name="checkmark-circle"
-                        aria-hidden="true"
-                      ></ion-icon>
-                    </div>
-
-                    <h3 className="title-md card-title">
-                      <a href="#" className="link:hover">
-                        Animations Collection
-                      </a>
-                    </h3>
-
-                    <p className="label-md card-author">
-                      by{" "}
-                      <a href="#" className="link">
-                        @CrazyAnyone
-                      </a>
-                    </p>
-
-                    <p className="card-text">25 Items</p>
-                  </div>
-                </div>
-              </li>
+                  </li>
+                ))}
+              </Slider>
             </ul>
 
-            <button
-              className="slider-btn prev"
-              aria-label="Slide to previous item"
-              data-slider-prev
-            >
+            {/* <button className="slider-btn prev">
               <ion-icon name="chevron-back-sharp" aria-hidden="true"></ion-icon>
             </button>
 
-            <button
-              className="slider-btn next"
-              aria-label="Slide to next item"
-              data-slider-next
-            >
+            <button className="slider-btn next">
               <ion-icon
                 name="chevron-forward-sharp"
                 aria-hidden="true"
               ></ion-icon>
-            </button>
+            </button> */}
           </div>
 
           <a href="#" className="btn-link link:hover">
---
+++ src/data.jsx
@@ -0,0 +1,44 @@
+export const collection = [
+  {
+    name: "Digital Collection",
+    creator: "@CutieGirl",
+    featured_image: "./assets/images/showcase-1.jpg",
+    avatar: "./assets/images/avatar-1.jpg",
+    items: "24",
+  },
+  {
+    name: "GIF Collection",
+    creator: "@ButterFly",
+    featured_image: "./assets/images/showcase-2.jpg",
+    avatar: "./assets/images/avatar-2.jpg",
+    items: "45",
+  },
+  {
+    name: "Sports Collection",
+    creator: "@NorseQueen",
+    featured_image: "./assets/images/showcase-3.jpg",
+    avatar: "./assets/images/avatar-3.jpg",
+    items: "94",
+  },
+  {
+    name: "Photography Collection",
+    creator: "@BigBull",
+    featured_image: "./assets/images/showcase-4.jpg",
+    avatar: "./assets/images/avatar-4.jpg",
+    items: "67",
+  },
+  {
+    name: "Illustrations Collection",
+    creator: "@Angel",
+    featured_image: "./assets/images/showcase-5.jpg",
+    avatar: "./assets/images/avatar-5.jpg",
+    items: "56",
+  },
+  {
+    name: "Animations Collection",
+    creator: "@CrazyAnyone",
+    featured_image: "./assets/images/showcase-6.gif",
+    avatar: "./assets/images/avatar-6.gif",
+    items: "43",
+  },
+];
